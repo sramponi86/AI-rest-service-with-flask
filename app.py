b835a35aa44ca7509f7b97aa14a0b88cdf0ec968
Stefano Ramponi <stefano.ramponi.lav@gmail.com>
2024-05-10 10:10:05 +0200

    added htmls

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 Bootstrap(app)
 
+# prediction function
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 12)
+    loaded_model = pickle.load(open("model.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/result', methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)        
+        if int(result)== 1:
+            prediction ='Income more than 50K'
+        else:
+            prediction ='Income less that 50K'           
+        return render_template("result.html", prediction = prediction)
 
 if __name__ == '_main__':
     app.run(debug=True)
